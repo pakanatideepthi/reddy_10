0a33ead60f0e53b065bbee20a34d125ff29174ec
pakanatideepthi <31736745+pakanatideepthi@users.noreply.github.com>
2017-09-07 21:44:45 +0530

    Update GuessingGame.java

--- GuessingGame.java
+++ GuessingGame.java
@@ -4,15 +4,33 @@ class GuessingGame {
     public int max;
 
     public GuessingGame(int min, int max) {
+        Random random=new Random();
+        int answer=random.nextInt(100);
+        int guess=-1;
+        while(guess!=answer)
+        {
+            System.out.println("enter youranswer");
+            guess=scanner.nextInt();
     }
 
     public int guess() {
+        if(guess=answer)
+        {
+            System.out.println("correct,the number was"+answer);
         return 0;
     }
 
     public void lower() {
+        if(guess<answer)
+        {
+            System.out.println("too low,please try again");
+        }
     }
 
     public void higher() {
+        if(guess>answer)
+        {
+            System.out.println("too high,please try again");
+        }
     }
 }
